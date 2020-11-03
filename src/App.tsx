457d787318b0bf48a1e8d9835251cb4f158b9f47
Ricardo De Lima <ricardo@delima.org>
2020-11-03 07:34:41 -0500

    getting version information from the server

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,47 @@
-import React from 'react';
-import logo from './barnes_logo.png';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import logo from "./barnes_logo.png";
+import "./App.css";
 
 function App() {
+
+  const [server_time, setTime] = useState();
+  const [version_info, setVersion] = useState([{id: '', message:'', created_at:''}]);
+
+ 
+  useEffect(() => {
+
+    async function getServerTime() {
+      const result = await axios(
+        'http://127.0.0.1:8000/api/time/',
+      );
+      setTime(result.data);
+    }
+
+    async function getVersionInfo() {
+      const result = await axios(
+        'http://127.0.0.1:8000/api/version/',
+      );
+      console.log(result);
+      setVersion(result.data);
+    }
+
+    getServerTime();
+    getVersionInfo();
+  }, []);
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello World!
-        </p>
+        <img src={logo} className="App-logo" alt="logo" /> 
+        <p className="HelloWorld">Hello World! it's {server_time} at GIS server</p>
+        <p>GIS6 version information: {version_info[0].message} created at: {version_info[0].created_at}</p>
       </header>
+     
     </div>
   );
 }
