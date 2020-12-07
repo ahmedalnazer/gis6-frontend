850afb5773fc2c3ac6ace77b3a09fc9643dc95f1
Venkat Bhashyam <>
2020-12-07 09:45:39 -0500

    added ViewFilter and OrderTable components

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import "./App.css";
 
+import ViewFilter from './order/view_filter';
 import Landing from './toolbox/Landing';
 import lineChart1 from './toolbox/diagrams/lineChart/lineChart1';
 import lineChart2 from './toolbox/diagrams/lineChart/lineChart2';
@@ -14,15 +15,8 @@ import lineChart6 from './toolbox/diagrams/lineChart/lineChart6';
 function App() {
 	return (
 		<Router>
-			<a href="/">Home</a>
-			<a href="/linechart1/">LineChart1(recharts)</a>
-			<a href="/linechart2/">LineChart2(chart.js)</a>
-			<a href="/linechart3/">LineChart3(victory)</a>
-			<a href="/linechart4/">LineChart4(google)</a>
-			<a href="/linechart5/">LineChart5(react-vis)</a>
-			<a href="/linechart6/">LineChart6(echarts)</a>
-
 			<Route path="/" exact component={Landing} />
+			<Route path="/order/list" exact component={ViewFilter} />
 			<Route path="/linechart1/" exact component={lineChart1} />
 			<Route path="/linechart2/" exact component={lineChart2} />
 			<Route path="/linechart3/" exact component={lineChart3} />
