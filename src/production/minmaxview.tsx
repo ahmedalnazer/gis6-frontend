31f3d7ce4d0cedbad91ccd62cbc2fac2192cf2b9
gsmith-bgi <gsmith@synventive.com>
2020-12-09 15:03:52 -0500

    Added grid layout to center the text

--- src/production/minmaxview.tsx
+++ src/production/minmaxview.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
 import { styled } from '@material-ui/core';
@@ -14,14 +15,14 @@ class MinMaxView extends React.Component
 		// need to consolidate styles between here and OrderList
 		const MyCard = styled(Card)(
 		{
-			borderColor: 'coral',
-			margin: 'auto',
+			alignItems: 'center',
 			border: 1,
+			borderColor: 'coral',
 			borderRadius: 3,
 			boxShadow: '1 0px 0px 0px rgba(51, 51, 255, .8)',
+			margin: 'auto',
+			padding: '0 0px',
 			width: 300,
-			alignItems: 'center',
-			padding: '0 0px'
 		});
 		
 		const MyCardHeader = styled(CardHeader)(
@@ -32,7 +33,6 @@ class MinMaxView extends React.Component
 		
 		const MyCardContent = styled(CardContent)(
 		{
-			backgroundColor: 'white',
 			color: 'black',
 		});
 		
@@ -46,25 +46,40 @@ class MinMaxView extends React.Component
 			fontSize: 16,
 		});
 		
-		//const classes = useStyles();
+		const MyGridItem = styled(Grid)(
+		{
+			textAlign: 'center',
+		});
 
 		return (
 			<div style={{padding:30}}>
 				<div style={{padding: 20}}>
 					<MyCard>
-						<MyCardHeader title="Temperature" subheader="Things are heating up!"/>
+						<MyCardHeader title="Temperature"/>
 						<MyCardContent>
-							<MajorTypography><span id="maxTemp">30&deg;C</span></MajorTypography>
-							<MinorTypography>Highest:&nbsp;<span id="maxTempLocale">Zone 10</span></MinorTypography>
-							<br/>
-							<MajorTypography><span id="minTemp">25&deg;C</span></MajorTypography>
-							<MinorTypography>Lowest:&nbsp;<span id="minTempLocale">Zone 5</span></MinorTypography>
-							<br/>
-							<MinorTypography>
-								<span id="material">Polypropylene</span>
-								&nbsp;|&nbsp;
-								Range&nbsp;<span id="minMaterialTemp">250</span>-<span id="maxMaterialTemp">270&deg;C</span>
-							</MinorTypography>
+							<Grid container direction="column">
+								<MyGridItem item xs>
+									<MajorTypography><span id="maxTemp">30&deg;C</span></MajorTypography>
+								</MyGridItem>
+								<MyGridItem item xs>
+									<MinorTypography>Highest:&nbsp;<span id="maxTempLocale">Zone 10</span></MinorTypography>
+								</MyGridItem>
+								<Grid item xs>&nbsp;</Grid>
+								<MyGridItem item xs>
+									<MajorTypography><span id="minTemp">25&deg;C</span></MajorTypography>
+								</MyGridItem>
+								<MyGridItem item xs>
+									<MinorTypography>Lowest:&nbsp;<span id="minTempLocale">Zone 5</span></MinorTypography>
+								</MyGridItem>
+								<Grid item xs>&nbsp;</Grid>
+								<MyGridItem item xs>
+									<MinorTypography>
+										<span id="material">Polypropylene</span>
+										&nbsp;|&nbsp;
+										Range&nbsp;<span id="minMaterialTemp">250</span>-<span id="maxMaterialTemp">270&deg;C</span>
+									</MinorTypography>
+								</MyGridItem>
+							</Grid>
 						</MyCardContent>
 					</MyCard>
 				</div>
