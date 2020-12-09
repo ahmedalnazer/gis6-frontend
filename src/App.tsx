d26d7cbea785b7157e5bdf982fd663dc07f624ee
Karl Tsai <ktsai@bginc.com>
2020-12-09 00:13:34 -0500

    initial work

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import "./App.css";
 
 import ViewFilter from './order/view_filter';
+import MinMaxView from './production/minmaxview';
 import Landing from './toolbox/Landing';
 import lineChart1 from './toolbox/diagrams/lineChart/lineChart1';
 import lineChart2 from './toolbox/diagrams/lineChart/lineChart2';
@@ -17,6 +18,7 @@ function App() {
 		<Router>
 			<Route path="/" exact component={Landing} />
 			<Route path="/order/list" exact component={ViewFilter} />
+			<Route path="/minmax/" exact component={MinMaxView} />
 			<Route path="/linechart1/" exact component={lineChart1} />
 			<Route path="/linechart2/" exact component={lineChart2} />
 			<Route path="/linechart3/" exact component={lineChart3} />
---
+++ src/production/minmax.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Card from '@material-ui/core/Card';
+
+
+class MinMax extends React.Component {
+    state = { info: [], isLoading: false };
+
+    componentDidMount() {
+        console.log("mounted");
+        this.doFetch();
+    }
+
+    componentDidUpdate() {
+        console.log("updated min/max");
+    }
+
+    doFetch() {
+        console.log("fetching");
+        const apiUrl = 'http://localhost:8000/system1/getminmax';
+        this.setState({isLoading: true});
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(info => this.setState({info: info, isLoading: false}))
+    }
+
+    render() {
+        console.log("current state: " + JSON.stringify(this.state));
+        if (this.state.isLoading) {
+            return <p>Table loading...</p>;
+        }
+
+        return (
+            <Card style={{ width: 800, margin: 'auto', boxShadow: '0 1px 1px 1px rgba(51, 51, 255, .8)' }}>
+                <Table style={{ width: 800, alignItems: 'center', margin: 'auto'}}>
+                <TableHead>
+                <TableRow>
+                        <TableCell style={{textAlign: 'center', padding: '5px'}}>{this.state.info['min']}</TableCell>
+                        <TableCell style={{textAlign: 'center', padding: '5px'}}>{this.state.info['max']}</TableCell>
+                        <TableCell style={{textAlign: 'center', padding: '5px'}}>{this.state.info['min_zone']}</TableCell>
+                        <TableCell style={{textAlign: 'center', padding: '5px'}}>{this.state.info['max_zone']}</TableCell>
+                </TableRow>
+                </TableHead>
+                </Table>
+            </Card>
+        );
+    }
+}
+
+export default MinMax;
