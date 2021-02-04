c2609fe9e7ddff4aadabe4d86500bd18551df5b8
“NiteshJayshwal” <NJayshwal@bginc.com>
2021-02-04 09:50:07 -0800

    GIS6-832 Added fields in boost dialog

--- app/src/components/taskbars/commands/Boost.svelte
+++ app/src/components/taskbars/commands/Boost.svelte
@@ -1,18 +1,44 @@
 <script>
   import Selector from '../Selector'
-  import { Modal } from 'components'
+  import { Modal, Input } from 'components'
   import _ from 'data/language'
+  import { notify } from 'data/'
   export let onClose
+
+  let auto = 104
+  let timeout = 0
+  let time = 0
+  let manualBoost
+  let recoveryTime
 
   const boost = zones => {
     console.log(zones)
     // set zones off
+    notify.success($_('Boost applied'))
   }
 </script>
 
 <Modal title={$_('Boost')} {onClose}>
   <Selector onSubmit={boost} onDone={onClose}>
     <h2>Edit</h2>
-    Controls
+    <div class='grid'>
+      <Input type='number' bind:value={auto} label='{$_('Auto Standby')}  (&#176;C)' />
+      <Input type='number' bind:value={timeout} label='{$_('Standby Timeout (min)')}' />
+      <Input type='number' bind:value={time} label='{$_('Time (sec)')}' />
+    </div>
+
+    <div class='grid'>
+      <Input type='number' bind:value={manualBoost} label='{$_('Manual Boost')}' />
+      <Input type='number' bind:value={recoveryTime} label='{$_('Recovery Time (min)')}' />
+    </div>
   </Selector>
 </Modal>
+
+<style>
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 16px;
+    margin-bottom: 30px;
+  }
+</style>
