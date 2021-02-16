2d0ca4379ace7fb03bcb094f511243ac2b7c3b57
Monu Chacko <monuchacko@gmail.com>
2021-02-16 10:07:51 -0500

    style changes

--- app/src/screens/analysis/ActiveAnalysis.svelte
+++ app/src/screens/analysis/ActiveAnalysis.svelte
@@ -1,49 +1,53 @@
 <script>
-  import { Progress } from 'components'
-  import faultAnalysis from 'data/analysis/fault'
-  import wiringAnalysis from 'data/analysis/wiring'
-  import _ from 'data/language';
-  import Error from './Error.svelte'
-import TestResults from './TestResults.svelte';
+  import { Progress } from "components";
+  import faultAnalysis from "data/analysis/fault";
+  import wiringAnalysis from "data/analysis/wiring";
+  import _ from "data/language";
+  import Error from "./Error.svelte";
+  import TestResults from "./TestResults.svelte";
 
-  export let type = 'fault'
+  export let type = "fault";
 
   let analyses = {
     fault: faultAnalysis,
-    wiring: wiringAnalysis
-  }
+    wiring: wiringAnalysis,
+  };
 
-  $: analysis = analyses[type]
-
+  $: analysis = analyses[type];
 </script>
 
-<div class='analysis'>
-  {#if $analysis.status == 'complete'}
+<div class="analysis">
+  {#if $analysis.status == "complete"}
     <TestResults analysis={$analysis} />
   {:else}
-    <div class='status'>
-      <h2>{$_('Test Status')}</h2>
-      <div class='status-wrapper'>
-        <div class='text'>
-          <p>{$_('All zones off.')}</p>
+    <div class="status">
+      <h2>{$_("Test Status")}</h2>
+      <div class="status-wrapper">
+        <div class="text">
+          <p>{$_("All zones off.")}</p>
           <p>{$analysis.status_message}</p>
         </div>
-        <div class='progress'>
+        <div class="progress">
           <p>{$analysis.progress_message}</p>
-          <Progress current={$analysis.progress} background='white'/>
+          <Progress current={$analysis.progress} background="white" />
         </div>
       </div>
     </div>
   {/if}
 
-  <div class='errors'>
+  <div class="errors">
     {#if $analysis.errors.length}
-      <div class='table-header'>
-        <p>{$_('Zone')}</p>
-        <p>{$_('Issue')}</p>
+      <div class="table-header">
+        <div class="name">
+          <p>{$_("Zone")}</p>
+        </div>
+        <div class="thumb">
+          <p>{$_("Issue")}</p>
+        </div>
+        <div class='desciption'>&nbsp;</div>
       </div>
       {#each $analysis.errors as error}
-        <Error error={error} />
+        <Error {error} />
       {/each}
     {/if}
   </div>
@@ -62,7 +66,7 @@ import TestResults from './TestResults.svelte';
     grid-template-columns: 1fr 1fr;
   }
   .progress p {
-   text-align: right;
+    text-align: right;
   }
 
   .errors {
@@ -71,9 +75,11 @@ import TestResults from './TestResults.svelte';
 
   .table-header {
     display: grid;
-    grid-template-columns: 100px 1fr;
+    grid-template-columns: 150px 100px 1fr;
     position: sticky;
     font-weight: 700;
     border-bottom: 1px solid #333333;
+    padding: 10px;
   }
+  
 </style>
