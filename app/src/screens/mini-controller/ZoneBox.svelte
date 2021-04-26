951fd6247570b7bb2d39c5f45f70d44e69baebc6
Chacko <uzedo@bgusi0007.lan>
2021-04-26 15:13:43 -0400

    updates

--- app/src/screens/mini-controller/ZoneBox.svelte
+++ app/src/screens/mini-controller/ZoneBox.svelte
@@ -22,8 +22,11 @@
 
   $: on = settings.on
   $: locked = settings.locked
-  $: boost = settings.boost
-  $: standby = settings.standby
+  // $: locked = true //TEST
+  // $: boost = settings.boost
+  $: boost = false //TEST
+  // $: standby = settings.standby
+  $: standby = true //TEST
 
   $: falling = zone.falling
   $: rising = zone.rising
@@ -46,11 +49,150 @@
     activeSetpointEditor.set('setpoint')
   }
 
+  console.log(settings)
+
+
   // $: {if(zone.hasTempAlarm || zone.hasPowerAlarm) console.log(zone.alarms)}
 </script>
 
 
 <div on:click={click} class:active class='rb-box zone-box' data-id={zone.id} data-group={group && group.id}>
+  <div class='group-colors'>
+    {#each tabs as t }
+      <div class='color-tab' style='background:{t}' />
+    {/each}
+  </div>
+
+  <div class='details' class:off={!on}>
+    {#if !on && !locked}
+      <div class='overlay' />
+    {/if}
+  </div>
+
+  <div class='name'>
+    {zone.name}
+  </div>
+
+  <div class='temp'>
+    <div class="icons-sec">sd</div>
+    <div class="tmp">
+      <div class='actual'>
+        {Math.round((zone.actual_temp || 0) / 10)}&deg;<span class='temp-type'>F</span>
+      </div>
+      <div class='setpoint'>
+        {#if manual}
+          <span class='manual'></span>
+        {:else}
+          {setpoint / 10 || '-'}&deg;<span class='temp-type'>F</span>
+        {/if}
+      </div>
+    </div>
+  </div>
+
+  <div class='power' class:powerWarning class:powerError>
+    {#if on}
+      <div class='percent'>{((zone.actual_percent || 0) / 10).toFixed(1)}%</div>
+      <div class='amps'>{((zone.actual_current || 0) / 10).toFixed(2).padStart(5, '0')} A</div>
+    {/if}
+  </div>
+
+</div>
+
+
+
+
+
+
+<div on:click={click} class:active class='rb-box zone-box' data-id={zone.id} data-group={group && group.id}>
+  
+  <div class='group-colors'>
+    {#each tabs as t }
+      <div class='color-tab' style='background:{t}' />
+    {/each}
+  </div>
+
+  <div class='details' class:off={!on}>
+    {#if !on && !locked}
+      <div class='overlay' />
+    {/if}
+  </div>
+
+  <div class='name'>
+    {zone.name}
+  </div>
+
+  <div class='temp' class:tempWarning class:tempError>
+
+    <div class='deviation-icon'>
+      {#if boost || standby}
+        <div class='animated' class:boost class:standby>
+          <Icon icon='boost' color='var(--warning)' />
+          <Icon icon='boost' color='var(--warning)' />
+          <div class='gradient-overlay' />
+        </div>
+      {:else}
+        {#if rising}
+          <Icon icon='up' color='white' />
+        {/if}
+        {#if falling}
+          <Icon icon='down' color='white' />
+        {/if}
+      {/if}
+
+      <div class='power' class:powerWarning class:powerError>
+        {#if locked}
+          <div class='icon-container'>
+            <Icon icon='lock' color='var(--blue)' />
+          </div>
+        {:else}
+          <div class='icon-container off-circle'>
+            <div class='circle' />
+          </div>
+        {/if}
+      </div>
+
+    </div>
+
+    <div>
+      <div class='actual'>
+        {Math.round((zone.actual_temp || 0) / 10)}&deg;<span class='temp-type'>F</span>
+      </div>
+      <div>
+        {#if on && zone.settings.sealed}
+          <div class='icon-container sealed'>
+            <div class='sealed-circle'>
+              <div class='sealed-line' />
+            </div>
+          </div>
+        {:else if on}
+          <div class='setpoint'>
+            {#if manual}
+              <span class='manual'></span>
+            {:else}
+              {setpoint / 10 || '-'}&deg;<span class='temp-type'>F</span>
+            {/if}
+          </div>
+        {/if}
+      </div>
+    </div>
+
+  </div>
+
+
+  <div class='power' class:powerWarning class:powerError>
+    {#if on}
+      <div class='percent'>{((zone.actual_percent || 0) / 10).toFixed(1)}%</div>
+      <div class='amps'>{((zone.actual_current || 0) / 10).toFixed(2).padStart(5, '0')} A</div>
+    {/if}
+  </div>
+
+</div>
+
+
+
+
+
+<!-- <div on:click={click} class:active class='rb-box zone-box' data-id={zone.id} data-group={group && group.id}>
   <div class='group-colors'>
     {#each tabs as t }
       <div class='color-tab' style='background:{t}' />
@@ -114,10 +256,11 @@
       {/if}
     </div>
   </div>
-</div>
+</div> -->
 
 
 <style lang="scss">
+
   .group-colors {
     display: flex;
   }
@@ -138,12 +281,14 @@
     background: rgba(255, 255, 255, .5);
   }
   .zone-box {
-    background: var(--blue);
+    background: var(--white);
+    // background: var(--blue);
     box-shadow: var(--shadow);
     border-radius: 0px 0px 3px 3px;
     margin: 4px;
     margin-bottom: 8px;
-    color: white;
+    // color: white;
+    color: var(--blue);
   }
   .active {
     border: 4px solid var(--selected);
@@ -182,6 +327,7 @@
     .setpoint, .temp-type {
       font-size: 11px;
       font-weight: 400;
+      float: right;
     }
   }
 
@@ -235,27 +381,28 @@
   }
 
   .deviation-icon :global(svg) {
-    width: 8px;
+    width: 14px;
   }
 
   @keyframes boostAnimation {
     0% {bottom: -100%}
     100% {bottom: 0%}
   }
-
+  
   .animated {
     display:flex;
     flex-direction: column;
     margin: 2px;
     position: relative;
     overflow: hidden;
+    float: left;
     :global(svg) {
-      width: 8px;
-      height: 8px;
+      width: 14px;
+      height: 14px;
     }
     .gradient-overlay  {
       animation: boostAnimation 1s infinite linear;
-      background: linear-gradient(var(--blue), transparent, transparent, var(--blue)) repeat;
+      background: linear-gradient(var(--pale), transparent, transparent, var(--pale)) repeat;
       background-size: 100% 50%;
       background-repeat: repeat;
       background-position: 0, 0;
